6b864f409df19e21dc60391f20612fbf553cbef9
DSgenes <155302928+DSgenes@users.noreply.github.com>
2024-02-17 14:44:59 +0500

    Add files via upload

---
+++ Logistic regression in R.R
@@ -0,0 +1,53 @@
+###Logistic Regression###
+#  Logistic regression is a technique for modeling the outcome of a binary variable on the 
+#  basis of some explainers that could be quantitative or qualitative 
+
+
+
+library(tidyverse)
+theme_set(theme_minimal())
+admissions <- read.csv(file.choose(), header = T)
+View(admissions)
+
+ggplot(admissions, aes(x = gpa, 
+                       y = admitted)) +
+  geom_jitter(height = .05, 
+              alpha = .1)
+
+model <- glm(admitted ~ gpa, 
+             data = admissions,
+             family = "binomial")
+summary(model)
+
+ggplot(admissions, aes(x = gpa, 
+                       y = admitted)) +
+  geom_jitter(height = .05, 
+              alpha = .1) +
+  geom_smooth(method = "glm",
+              method.args = list(family = "binomial"),
+              se = FALSE)
+
+adm_sum <- admissions %>% 
+  group_by(gpa) %>% 
+  summarize(prop_adm = mean(admitted),
+            count = n())
+adm_sum
+
+ggplot(adm_sum, aes(x = gpa, 
+                    y = prop_adm)) +
+  geom_point()
+
+model2 <- glm(prop_adm ~ gpa, 
+              family = "binomial",
+              data = adm_sum, 
+              weights = count)
+summary(model2)
+
+
+ggplot(adm_sum, aes(x = gpa, 
+                    y = prop_adm,
+                    weight = count)) +
+  geom_point() +
+  geom_smooth(method = "glm",
+              se = FALSE,
+              method.args = list(family = "binomial"))
